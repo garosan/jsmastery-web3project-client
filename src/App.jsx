f1535b9ffecf1710a4b7d12bb86b977e505e0ec4
Edgar Sánchez <garosanchez27@gmail.com>
2022-02-17 21:47:39 -0600

    Fix Tailwind configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import './index.css';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <p className="font-extrabold">This is P1</p>
+        <p className="font-thin">This is P2</p>
       </header>
     </div>
   );
