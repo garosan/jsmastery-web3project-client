4b78032fb812f860cd29b52c6c924054338388b7
Edgar Sánchez <garosanchez27@gmail.com>
2022-02-17 21:29:05 -0600

    Configure vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
